c27225a2cdbb17fb6333e4b4ce128d294b3f97e5
wanyu0409 <jasperwendy0409@gmail.com>
2017-10-24 11:50:25 +0800

    calaulate bmi

--- app/src/main/java/com/wendy/bmi/MainActivity.java
+++ app/src/main/java/com/wendy/bmi/MainActivity.java
@@ -2,6 +2,9 @@ package com.wendy.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void bmi(View view){
+//        System.out.println("What???");
+        Log.d("MainActivity", "testing bmi method");
+        EditText edWeight = (EditText) findViewById(R.id.ed_weight);
+        EditText edHeight = (EditText) findViewById(R.id.ed_height);
+        float weight = Float.parseFloat(edWeight.getText().toString());
+        float height = Float.parseFloat(edHeight.getText().toString());
     }
 }
