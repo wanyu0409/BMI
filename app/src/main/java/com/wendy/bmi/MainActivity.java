b15a8439665c1c75394932b7b2f771d74fdeafa7
wanyu0409 <jasperwendy0409@gmail.com>
2017-11-07 12:04:41 +0800

    onClick

--- app/src/main/java/com/wendy/bmi/MainActivity.java
+++ app/src/main/java/com/wendy/bmi/MainActivity.java
@@ -9,20 +9,22 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    String s = new String("abc");
-    View.OnClickListener listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
 
-        }
-    };
-    //匿名類別
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button bhelp = (Button) findViewById(R.id.b_help);
-        bhelp.setOnClickListener(listener);
+        bhelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("xxx")
+                        .setMessage("BMI原來的設計是一個用於公眾健康研究的統計工具。當需要知道肥胖是否為某一疾病的致病原因時，可以把病人的身高及體重換算成BMI，再找出其數值及病發率是否有線性關連。由於BMI主要反應整體體重，無法區別體重中體脂肪組織與非脂肪組織（包括肌肉、器官），同樣身高體重的人可算出相同的BMI，但其實脂肪量不同，因此其實BMI是整體營養狀態的指標。以往拿來做為肥胖的指標，是因發現BMI與體脂肪在統計上有高度相關；但在同樣BMI之下，仍會有體脂肪率的差異。")
+                        .setPositiveButton("OK", null)
+                        .show();
+            }
+        });
         //getResources().getString(R.string.app_name);
     }
     public void bmi(View view){
